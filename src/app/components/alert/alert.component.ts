9bf8f476c1a09539d73db10503d383b5c53fab10
Myron Castillo <mcastillo@catalyte.io>
2019-08-13 21:04:12 -0500

    Set up template and logic

--- src/app/components/alert/alert.component.ts
+++ src/app/components/alert/alert.component.ts
@@ -1,15 +1,59 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy, Input } from '@angular/core';
+import { Subscription } from 'rxjs';
+
+import { Alert, AlertType } from '../../models/Alert';
+import { AlertService } from '../../services/alert.service';
 
 @Component({
   selector: 'app-alert',
   templateUrl: './alert.component.html',
   styleUrls: ['./alert.component.scss']
 })
-export class AlertComponent implements OnInit {
+export class AlertComponent implements OnInit, OnDestroy {
+  @Input() id: string;
 
-  constructor() { }
+  alerts: Alert[] = [];
+  subscription: Subscription;
+
+  constructor(private alertService: AlertService) { }
 
   ngOnInit() {
+    this.subscription = this.alertService.onAlert(this.id).subscribe(alert => {
+      if (!alert.message) {
+        // clear alerts when an empty alert is received
+        this.alerts = [];
+        return;
+      }
+
+      // add alert to array
+      this.alerts.push(alert);
+    });
   }
 
+  ngOnDestroy() {
+    // unsubscribe
+    this.subscription.unsubscribe();
+  }
+
+  removeAlert(alert: Alert) {
+    // remove specified alert
+    this.alerts = this.alerts.filter(x => x !== alert);
+  }
+
+  cssClass(alert: Alert) {
+    if (!alert) {
+      return;
+    }
+
+    switch(alert.type) {
+      case AlertType.Success:
+        return 'alert alert-success';
+      case AlertType.Error:
+        return 'alert alert-danger';
+      case AlertType.Info:
+        return 'alert alert-info';
+      case AlertType.Warning:
+        return 'alert alert-warning';
+    }
+  }
 }
